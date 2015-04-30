a603fd12fd45f79210b124c549330815b5209b88
Paul Tagliamonte <tag@pault.ag>
2015-04-29 23:30:49 -0400

    Remove hardcoded stuff

--- main.go
+++ main.go
@@ -1,37 +1,63 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net"
+	"os"
 
 	"github.com/paultag/go-dictd/database"
 	"github.com/paultag/go-dictd/dictd"
 )
 
+/* Used for the JSON file that we can load off of */
+type Configuration struct {
+	Name string
+	Info string
+
+	Databases []struct {
+		Name string
+		Path string
+		Desc string
+	}
+}
+
+/* Given a config, load them up! */
+func loadDatabases(path string) (config *Configuration, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return
+	}
+
+	decoder := json.NewDecoder(file)
+	config = new(Configuration)
+	err = decoder.Decode(&config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
 func main() {
-	server := dictd.NewServer("pault.ag")
-	jargon, err := database.NewLevelDBDatabase(
-		"/home/tag/jargon.ldb",
-		"jargon file",
-	)
-	congress, err := database.NewLevelDBDatabase(
-		"/home/tag/congress.ldb",
-		"congress words",
-	)
-	debian, err := database.NewLevelDBDatabase(
-		"/home/tag/debian.ldb",
-		"debian words",
-	)
+	config, err := loadDatabases("config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	urbanDB := database.UrbanDictionaryDatabase{}
+	server := dictd.NewServer(config.Name)
 
-	server.RegisterDatabase(jargon, "jargon")
-	server.RegisterDatabase(congress, "congress")
-	server.RegisterDatabase(debian, "debian")
-	server.RegisterDatabase(&urbanDB, "urban")
+	for _, dbConfig := range config.Databases {
+		db, err := database.NewLevelDBDatabase(
+			dbConfig.Path,
+			dbConfig.Desc,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		server.RegisterDatabase(db, dbConfig.Name)
+	}
 
 	link, err := net.Listen("tcp", ":2628")
 	if err != nil {
